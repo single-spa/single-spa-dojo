60a9250297a535bb190d1cab94c9e1e586eeb8cb
VigneshRamamoorthy1992 <31540363+VigneshRamamoorthy1992@users.noreply.github.com>
2020-02-13 10:20:55 +0530

    mount and unmount changes
    addition of mounting dojo application and temp solution for unmounting

--- src/single-spa-dojo.js
+++ src/single-spa-dojo.js
@@ -1,75 +1,78 @@
 // opts are what is passed to singleSpaDojo({...})
-const defaultOpts = {};
+const defaultOpts = {
+	renderer: null,
+	mountOptions: {
+		registry: null
+	}
+};
 
 export default function singleSpaDojo(userOpts) {
-  if (typeof userOpts !== "object") {
-    throw new Error(`single-spa-dojo requires a configuration object`);
-  }
+	if (typeof userOpts !== "object") {
+		throw new Error(`single-spa-dojo requires a configuration object`);
+	}
 
-  const opts = {
-    ...defaultOpts,
-    ...userOpts
-  };
+	const opts = {
+		...defaultOpts,
+		...userOpts
+	};
 
-  const lifecycles = {
-    bootstrap: bootstrap.bind(null, opts),
-    mount: mount.bind(null, opts),
-    unmount: unmount.bind(null, opts)
-  };
+	const lifecycles = {
+		bootstrap: bootstrap.bind(null, opts),
+		mount: mount.bind(null, opts),
+		unmount: unmount.bind(null, opts)
+	};
 
-  return lifecycles;
+	return lifecycles;
 }
 
 function bootstrap(opts, props) {
-  return Promise.resolve();
+	return Promise.resolve();
 }
 
 function mount(opts, props) {
-  return Promise.resolve().then(() => {
-    const domElementGetter = chooseDomElementGetter(opts, props);
-    const domElement = domElementGetter();
-
-    // here is where we should do something like new Registry(), renderer().mount({register})
-    // We should mount dojo  into the domElement
-  });
-}
+	return Promise.resolve().then(() => {
+		const domElementGetter = chooseDomElementGetter(opts, props);
+		const domElement = getRootDomEl(domElementGetter, props);
+		opts.mountOpts.domNode = domElement
+		opts.renderer.mount(opts.mountOptions)
+	}
 
 function unmount(opts, props) {
-  return Promise.resolve().then(() => {
-    // here is where we tell dojo to unmount
-  });
-}
+			return Promise.resolve().then(() => {
+				renderer(() => <div style="display: none" />).mount({ domNode });
+			});
+		}
 
 function chooseDomElementGetter(opts, props) {
-  props = props && props.customProps ? props.customProps : props;
-  if (props.domElement) {
-    return () => props.domElement;
-  } else if (props.domElementGetter) {
-    return props.domElementGetter;
-  } else if (opts.domElementGetter) {
-    return opts.domElementGetter;
-  } else {
-    return defaultDomElementGetter(props);
-  }
-}
+			props = props && props.customProps ? props.customProps : props;
+			if (props.domElement) {
+				return () => props.domElement;
+			} else if (props.domElementGetter) {
+				return props.domElementGetter;
+			} else if (opts.domElementGetter) {
+				return opts.domElementGetter;
+			} else {
+				return defaultDomElementGetter(props);
+			}
+		}
 
 function defaultDomElementGetter(props) {
-  const appName = props.appName || props.name;
-  if (!appName) {
-    throw Error(
-      `single-spa-dojo was not given an application name as a prop, so it can't make a unique dom element container for the dojo application`
-    );
-  }
-  const htmlId = `single-spa-application:${appName}`;
+			const appName = props.appName || props.name;
+			if (!appName) {
+				throw Error(
+					`single-spa-dojo was not given an application name as a prop, so it can't make a unique dom element container for the dojo application`
+				);
+			}
+			const htmlId = `single-spa-application:${appName}`;
 
-  return function defaultDomEl() {
-    let domElement = document.getElementById(htmlId);
-    if (!domElement) {
-      domElement = document.createElement("div");
-      domElement.id = htmlId;
-      document.body.appendChild(domElement);
-    }
+			return function defaultDomEl() {
+				let domElement = document.getElementById(htmlId);
+				if (!domElement) {
+					domElement = document.createElement("div");
+					domElement.id = htmlId;
+					document.body.appendChild(domElement);
+				}
 
-    return domElement;
-  };
-}
+				return domElement;
+			};
+		}
